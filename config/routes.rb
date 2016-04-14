0eccb791e98d60601a5d8342ebd81751dcb6405c
tankietkt <tankietkt@gmail.com>
2016-04-14 20:01:28 +0700

    add view purchase

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'purchase_orders/index'
+
   resources :inventory_items
+  resources :purchase_orders
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
